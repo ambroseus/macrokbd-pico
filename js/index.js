179274ddc027b38a8721669508b55c9a301c80ba
Eugene Samonenko <ambroseus@gmail.com>
2024-02-20 15:14:25 +0200

    js neopixel

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 const { NeoPixel } = require('neopixel')
 const BOARD_NEOPIXEL_PIN = 23
-
 const np = new NeoPixel(BOARD_NEOPIXEL_PIN, 1)
+const Off = np.color(0, 0, 0)
+const Green = np.color(20, 40, 0)
+
 let on = false
 
 const toggle = () => {
-  const color = on ? np.color(20, 40, 0) : np.color(0, 0, 0)
-  np.setPixel(0, color)
-
+  np.setPixel(0, on ? Green : Off)
   np.show()
   on = !on
 }
