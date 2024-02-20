9e5915e9657d387b2403e6bd8aa114e40ad29e5e
Eugene Samonenko <ambroseus@gmail.com>
2024-02-20 14:30:44 +0200

    js init

---
+++ js/.eslintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  rules: {
+  },
+}
---
+++ js/index.js
@@ -0,0 +1,4 @@
+const led = 25
+pinMode(led, OUTPUT)
+
+setInterval(() => digitalToggle(led), 1000)
